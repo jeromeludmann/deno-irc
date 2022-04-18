ce7a8bb29fe21df3dcb97b7b5be57cb188c192c3
Jerome Ludmann <jerome.ludmann@gmail.com>
2022-04-18 16:39:16 +0200

    chore(deps): upgrade to std@0.135.0

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export {
   green,
   red,
   stripColor,
-} from "https://deno.land/std@0.134.0/fmt/colors.ts";
+} from "https://deno.land/std@0.135.0/fmt/colors.ts";
 export {
   assertArrayIncludes,
   assertEquals,
@@ -12,4 +12,4 @@ export {
   assertMatch,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.134.0/testing/asserts.ts";
+} from "https://deno.land/std@0.135.0/testing/asserts.ts";
