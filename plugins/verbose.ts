e76f7e5caecb46814e54912d37d3561eeed2f714
Jerome Ludmann <jerome.ludmann@gmail.com>
2023-09-28 13:47:32 +0200

    feat(plugins/verbose): add "raw", "formatted" and custom logger impl

--- plugins/verbose.ts
+++ plugins/verbose.ts
@@ -1,72 +1,150 @@
+import { CoreClient } from "../core/client.ts";
 import { type ClientError } from "../core/errors.ts";
 import { createPlugin } from "../core/plugins.ts";
+import { type AnyRawCommand } from "../core/protocol.ts";
 import { bold, dim, green, red } from "../deps.ts";
+
+interface RawLogPayload {
+  type: "raw_input" | "raw_output";
+  msg: string | null;
+}
+
+interface EventLogPayload {
+  type: "event";
+  event: string;
+  payload: unknown;
+}
+
+interface CommandLogPayload {
+  type: "command";
+  command: AnyRawCommand;
+  params: (string | undefined)[];
+}
+
+interface StateLogPayload {
+  type: "state";
+  state: CoreClient["state"];
+  key: keyof CoreClient["state"];
+  value: unknown;
+}
+
+type LogPayload =
+  | RawLogPayload
+  | EventLogPayload
+  | CommandLogPayload
+  | StateLogPayload;
+
+type LoggerImpl = (payload: LogPayload) => void;
 
 interface VerboseFeatures {
   options: {
-    /** Prints informations to output. */
-    verbose?: boolean;
+    /** Prints informations to output.
+     *
+     * - `"raw"`: received and sent raw IRC messages
+     * - `"formatted"`: formatted events, commands and state changes
+     * - `(payload: LogPayload) => void`: a custom logger implementation */
+    verbose?: "raw" | "formatted" | LoggerImpl;
   };
 }
 
-const DEFAULT_VERBOSE = false;
+// Default logger used with "raw" verbose value
+const defaultRawLogger: LoggerImpl = (payload) => {
+  switch (payload.type) {
+    case "raw_input": { // Prints received raw messages
+      if (payload.msg !== null) {
+        console.info(
+          dim("read"),
+          dim(bold("chunks")),
+          dim(JSON.stringify(payload.msg)),
+        );
+      }
+      break;
+    }
+
+    case "raw_output": { // Prints sent raw messages
+      if (payload.msg !== null) {
+        console.info(
+          dim("send"),
+          dim(bold("raw")),
+          dim(JSON.stringify(payload.msg)),
+        );
+      }
+      break;
+    }
+  }
+};
+
+// Default logger used with "formatted" verbose value
+const defaultFormattedLogger: LoggerImpl = (payload) => {
+  switch (payload.type) {
+    case "event": { // Prints emitted events
+      if (payload.event.startsWith("raw")) {
+        return;
+      }
+      if (payload.event === "error") {
+        const { type, name, message } = payload.payload as ClientError;
+        payload.payload = { type, name, message };
+      }
+      console.info("emit", bold(payload.event), payload.payload);
+      break;
+    }
+
+    case "command": { // Prints sent commands
+      console.info("send", bold(payload.command), payload.params);
+      break;
+    }
+
+    case "state": { // Prints state changes
+      const prev = JSON.stringify(payload.state[payload.key]);
+      const next = JSON.stringify(payload.value);
+      const label = bold(payload.key.toString());
+
+      if (prev !== next) {
+        console.info("diff", label, red(`- ${prev}`));
+        console.info("diff", label, green(`+ ${next}`));
+      }
+      break;
+    }
+  }
+};
 
 export default createPlugin(
   "verbose",
 )<VerboseFeatures>((client, options) => {
-  const enabled = options.verbose ?? DEFAULT_VERBOSE;
-  if (!enabled) return;
+  const getLoggerImpl = () => {
+    switch (options.verbose) {
+      case "raw":
+        return defaultRawLogger;
+      case "formatted":
+        return defaultFormattedLogger;
+      case undefined:
+        return undefined;
+      default:
+        return options.verbose;
+    }
+  };
 
-  // Prints received messages.
+  const loggerImpl = getLoggerImpl();
+  if (!loggerImpl) return;
 
   // deno-lint-ignore no-explicit-any
   client.hooks.afterCall("read" as any, (chunks: string | null) => {
-    if (chunks !== null) {
-      console.info(
-        dim("read"),
-        dim(bold("chunks")),
-        dim(JSON.stringify(chunks)),
-      );
-    }
+    loggerImpl({ type: "raw_input", msg: chunks });
   });
-
-  // Prints sent messages.
 
   client.hooks.afterCall("send", (raw) => {
-    if (raw !== null) {
-      console.info(dim("send"), dim(bold("raw")), dim(JSON.stringify(raw)));
-    }
+    loggerImpl({ type: "raw_output", msg: raw });
   });
-
-  // Prints sent commands.
 
   client.hooks.beforeCall("send", (command, ...params) => {
-    console.info("send", bold(command), params);
+    loggerImpl({ type: "command", command, params });
   });
-
-  // Prints emitted events.
 
   client.hooks.beforeCall("emit", (event, payload) => {
-    if (event.startsWith("raw")) {
-      return;
-    }
-    if (event === "error") {
-      const { type, name, message } = payload as ClientError;
-      payload = { type, name, message };
-    }
-    console.info("emit", bold(event), payload);
+    loggerImpl({ type: "event", event, payload });
   });
 
-  // Prints state changes.
-
   client.hooks.beforeMutate("state", (state, key, value) => {
-    const prev = JSON.stringify(state[key]);
-    const next = JSON.stringify(value);
-    const label = bold(key.toString());
-
-    if (prev !== next) {
-      console.info("diff", label, red(`- ${prev}`));
-      console.info("diff", label, green(`+ ${next}`));
-    }
+    loggerImpl({ type: "state", state, key, value });
   });
 });
