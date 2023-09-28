760fb22a1b5e95a0a3b4909428232009bb2833ac
Jerome Ludmann <jerome.ludmann@gmail.com>
2023-09-28 16:36:59 +0200

    refactor(deps): remove deprecated references

--- deps.ts
+++ deps.ts
@@ -5,11 +5,10 @@ export {
   red,
   stripColor,
 } from "https://deno.land/std@0.203.0/fmt/colors.ts";
-export {
-  assertArrayIncludes,
-  assertEquals,
-  assertExists,
-  assertMatch,
-  assertRejects,
-  assertThrows,
-} from "https://deno.land/std@0.203.0/testing/asserts.ts";
+
+export { assertArrayIncludes } from "https://deno.land/std@0.203.0/assert/assert_array_includes.ts";
+export { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+export { assertExists } from "https://deno.land/std@0.203.0/assert/assert_exists.ts";
+export { assertMatch } from "https://deno.land/std@0.203.0/assert/assert_match.ts";
+export { assertRejects } from "https://deno.land/std@0.203.0/assert/assert_rejects.ts";
+export { assertThrows } from "https://deno.land/std@0.203.0/assert/assert_throws.ts";
