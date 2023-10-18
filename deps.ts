49ff38b1ecdefc2bb73102a22e7cc6d57d46acaa
Isaac Aronson <i@pingas.org>
2023-10-18 18:08:11 -0500

    Remove dependency on 3rd party library for websockets

--- deps.ts
+++ deps.ts
@@ -8,14 +8,12 @@ export {
 
 export { assertArrayIncludes } from "https://deno.land/std@0.203.0/assert/assert_array_includes.ts";
 export { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+export { assertNotEquals } from "https://deno.land/std@0.203.0/assert/assert_not_equals.ts";
 export { assertExists } from "https://deno.land/std@0.203.0/assert/assert_exists.ts";
 export { assertMatch } from "https://deno.land/std@0.203.0/assert/assert_match.ts";
 export { assertRejects } from "https://deno.land/std@0.203.0/assert/assert_rejects.ts";
 export { assertThrows } from "https://deno.land/std@0.203.0/assert/assert_throws.ts";
+export { on } from "https://deno.land/std@0.92.0/node/events.ts";
 
 export { Queue } from "https://deno.land/x/queue@1.2.0/mod.ts";
-
-export type { WebSocketClient } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
-export { StandardWebSocketClient } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
-
 
